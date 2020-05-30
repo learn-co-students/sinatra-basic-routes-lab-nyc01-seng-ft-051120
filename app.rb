bd181d6ab8eae83e980c81797c4cbf7bd466d7c1
https://learn.co/christa-gammage <crgammage@gmail.com>
2020-05-30 11:40:38 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Christa"
+    end
+    get '/hometown' do
+        "My hometown is New York City"
+    end
+    get '/favorite-song' do
+        "My favorite song is Dive"
+    end
 end

03b3f16a7ff0bbb4e4a2d564d03ff604e0fa0eb8
callas4ever <thetseringnorbu@gmail.com>
2020-05-31 11:57:18 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        'My name is Tsering Norbu'
+    end
+    get '/hometown' do
+        "My hometown is lhasa"
+    end
+    get '/favorite-song' do
+        'My favorite song is Xany'
+    end
 end
